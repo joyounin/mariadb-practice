ef80a31c460cd5f01ae49129c27fb2a2ee8939f9
Joyounin <3723519@naver.com>
2023-01-06 15:10:47 +0900

    first commit

---
+++ sql연습/basic-query.sql
@@ -0,0 +1,47 @@
+show databases;
+
+use webdb;
+
+select version(), current_date(), now() from dual;
+
+
+-- 수학함수, 사칙 연산도 된다.
+select sin(pi() / 4 ), 1 + 2 * 3 - 4 / 5 from dual;
+
+-- 대소문자 구분 안한다.
+sElect VERSION(), current_date(), Now() from dual;
+
+-- 두개 동시에 실행가능(CLI에서 가능)
+select sin(pi() / 4 ); select 1 + 2 * 3 - 4 / 5 from dual;
+
+
+
+show tables;
+
+-- table 생성: DML
+create table pet (
+	name varchar(100),
+	owner varchar(20),
+	species varchar(20),
+	gender char(1),
+	birth date,
+	death date
+);
+
+-- schema 확인 테이블 구조
+desc pet;
+
+-- table 삭제
+drop table pet;
+show tables;
+
+-- insert: DML(C)
+insert into pet values('망고','조영인', 'f', '2014-11-10', 'cat', null);
+
+select * from pet;
+
+
+
+
+
+
